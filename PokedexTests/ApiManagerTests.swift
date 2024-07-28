7cda94acfcf5deb272e54cde25d258d159e233bd
João Pedro Cappelletto D'Agnoluzzo <joao.dagnoluzzo@gmail.com>
2024-07-28 00:35:56 -0300

    feat: added test on APIManager

--- PokedexTests/ApiManagerTests.swift
+++ PokedexTests/ApiManagerTests.swift
@@ -29,7 +29,7 @@ class ApiManagerTests: QuickSpec {
         }
         
         describe("requesting") {
-            context("when fetching a list of pokemon types") {
+            context("when fetchint poketypes and there's no results") {
                 beforeEach {
                     networkSession.data = "{\"count\":0, \"results\":[]}".data(using: .utf8)
                     networkSession.error = nil
@@ -54,6 +54,36 @@ class ApiManagerTests: QuickSpec {
                     expect(error).to(beNil())
                     expect(pokeType).toNot(beNil())
                     expect(pokeType?.count).to(equal(0))
+                    expect(pokeType?.results.count).to(equal(0))
+                }
+            }
+            
+            context("when fetching poketypes and there's a list of results") {
+                beforeEach {
+                    networkSession.data = TestHelper.pokeTypesResult()
+                    networkSession.error = nil
+                }
+                
+                it("should return the a list of decoded PokeTypeModel") {
+                    var pokeType: PokeTypeModel?
+                    var error: Error?
+                    
+                    waitUntil(timeout: .seconds(3)) { (done) in
+                        sut.fetchPokeTypes { (result) in
+                            switch result {
+                            case .success(let pokeTypeResponse):
+                                pokeType = pokeTypeResponse
+                            case .failure(let responseError):
+                                error = responseError
+                            }
+                            done()
+                        }
+                    }
+                    
+                    expect(error).to(beNil())
+                    expect(pokeType).toNot(beNil())
+                    expect(pokeType?.count).to(equal(3))
+                    expect(pokeType?.results.count).to(equal(3))
                 }
             }
         }
